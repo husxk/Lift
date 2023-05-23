34f65d803fc4dc110bda58a19f2cb23f686e49d6
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-05-23 22:21:13 +0200

    add Person class

---
+++ Src/Person/Person.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdint.h>
+
+class Person {
+    uint16_t weight;
+    uint32_t floor_number;
+
+    enum special_event_specifier
+    {
+        male = 1,
+        female = 2,
+        kid = 4
+    };
+
+    special_event_specifier specifier;
+
+
+public:
+    Person() = default;
+};
