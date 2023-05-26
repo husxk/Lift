af19eec655dc7af8fbdc9f31aa9892f7454693d7
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-05-27 01:32:15 +0200

    Add Female implementation

--- Src/Person/Female/Female.cpp
+++ Src/Person/Female/Female.cpp
@@ -1,1 +1,11 @@
 #include "Female.h"
+
+Female::Female(uint32_t floors_number, uint32_t current_floor)
+{
+    int decided = rand() % 100;
+    if(decided <= d_decided_percent) is_decided = true;
+    else is_decided = false;
+
+    set_weight();
+    set_floor(floors_number, current_floor);
+};
--- Src/Person/Female/Female.h
+++ Src/Person/Female/Female.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "../Person.h"
 
-class Female:Person{
+#include <iostream>
+
+const int d_decided_percent = 90; // % needed to be decided and not have special event
+
+class Female: protected Person{
     bool is_decided;
-    void change_floor_number();
-    void iteration();
+    special_event_specifier specifier = female;
+    //void change_floor_number();
 public:
-    Female() = default;
+    explicit Female(uint32_t, uint32_t);
+   // void iteration();
 };
