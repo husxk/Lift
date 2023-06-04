d776788021f28f7a052d003b712b2ee60c0c4848
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-06-04 02:51:48 +0200

    Add chechpoint at making simulation
    this checkpoint works for case when lift does not need to communicate with floors for moving between them. For now lift gets values for lift_queue only from people going in the lift, not from floors as well.

--- Src/Simulator/Simulator.cpp
+++ Src/Simulator/Simulator.cpp
@@ -1,11 +1,61 @@
 #include "Simulator.h"
 
+void Simulator::check_for_desired_floor()
+{
+    for (uint32_t i = 0; i < lift->people_in_lift.size(); i++) {
+        if (lift->people_in_lift.at(i).get()->get_person_floor() == lift->get_current_position())
+        {
+            lift->weight_update(lift->people_in_lift.at(i)->get_person_weight(), '-');
+            lift->delete_floor_in_queue(i);
+            lift->people_in_lift.erase(lift->people_in_lift.begin() + i);
+            i -= 1;
+        }
+    }
+}
+
+void Simulator::iteration()
+{
+    // iterate for all floors
+    for(int i = 0; i < settings->get_value("floor_number"); i++)
+        floors[i].iteration();
+
+    // checks if current floor and desired floor of any person in lift meets,
+    // yes -> erase from lift + update lift weight
+    check_for_desired_floor();
+
+    // while there is person at current floor and person can get in the lift (lift current weight + person weight <= max lift weight)
+    // adds person to lift
+    // TODO: factorise this to functions
+    while(!floors[lift->get_current_position()].is_person() // is_person() returns if person_queue at floor is empty
+            && lift->get_weight() + floors[lift->get_current_position()].get_first_person_in_queue()->get_person_weight() <= lift->get_max_weight())
+    {
+        lift->people_in_lift.push_back(floors[lift->get_current_position()].get_first_person_in_queue());
+        floors[lift->get_current_position()].delete_person_from_floor();
+        lift->weight_update(lift->people_in_lift.back()->get_person_weight(), '+');
+        lift->add_new_floor_to_queue(lift->people_in_lift.back()->get_person_floor());
+    }
+
+    std::cout << "floor: " << lift->get_current_position() << " weight: " << lift->get_weight() << " next: " << lift->people_in_lift.front()->get_person_floor();
+    std::cout << std::endl;
+
+    if(lift->is_queue_empty())
+    {
+        return;
+    }
+    else
+        lift->lift_move(); // TODO: add checking for 0 floors in queue
+}
+
+
 Simulator::Simulator() // TODO: add data collector class
 {
     settings = std::make_shared<Settings>();
     settings->get_settings();
 
-    if(settings->is_default)
+    if(settings->settings_default())
         lift = std::make_shared<Lift>();
     else lift = std::make_shared<Lift>(settings->get_value("lift_max_weight"), settings->get_value("floor_number"));
+
+    floors = std::make_unique<Floor[]>(settings->get_value("floor_number"));
+
 }
--- Src/Simulator/Simulator.h
+++ Src/Simulator/Simulator.h
@@ -2,15 +2,26 @@
 
 #include <memory>
 #include <iostream>
+#include <cstdlib>
 
 #include "../Lift/Lift.h"
 #include "../Settings/Settings.h"
+#include "../Floor/Floor.h"
+
+#include "../Person/Person.h"
+#include "../Person/Female/Female.h"
+#include "../Person/Male/Male.h"
+#include "../Person/Kid/Kid.h"
 
 class Simulator
 {
-    std::shared_ptr<Settings> settings;
-    std::shared_ptr<Lift> lift;
+    std::shared_ptr<Settings> settings; // TODO change to unique
+    std::unique_ptr<Floor[]> floors;
+    void check_for_desired_floor();
 
 public:
+    std::shared_ptr<Lift> lift; //TODO change to unique
     Simulator();
+
+    void iteration();
 };
