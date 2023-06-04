d4bf1b9d188f24a76af81243eddac3935a8a9840
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-06-04 02:23:20 +0200

    Change the way we are limiting randomising in Person

--- Src/Person/Person.cpp
+++ Src/Person/Person.cpp
@@ -26,7 +26,7 @@ void Person::set_floor(uint32_t floors_number, uint32_t current_floor) // TODO: 
 {
     do
     {
-        floor_number = rand() % (floors_number + 1); // floors_number is limiting to <0;9>, adding +1 to have <0;10>
+        floor_number = rand() % floors_number; // floors_number is limiting to <0;10>
     }while(floor_number == current_floor);  // it is not necessary to call lift for the same floor as you are
 }
 
