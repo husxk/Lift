634bfa53a6070312633893116910173b1450adfd
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-06-05 23:44:32 +0200

    simulation iterate by given number

--- Src/main.cpp
+++ Src/main.cpp
@@ -8,6 +8,9 @@ int main()
     srand(time(NULL));
     auto simulator = std::make_shared<Simulator>();
 
+    for(int i = 0; i < simulator->get_iterations() ; i++) {
+        simulator->iteration();
+    }
 
-
+    std::cout << std::endl << "end";
 }
