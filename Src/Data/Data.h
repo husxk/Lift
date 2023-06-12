c8d059de72f870186ec15e19dea99926a66679c6
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-06-12 04:40:39 +0200

    Add Data class

---
+++ Src/Data/Data.h
@@ -0,0 +1,16 @@
+
+#include <memory>
+
+class Data
+{
+
+    uint64_t people_served;
+    uint64_t random_event_male;
+    uint64_t random_event_female;
+    uint64_t random_event_kid;
+    bool is_successful;
+
+public:
+    Data() = default;
+    ~Data() = default;
+};
