87b345f5bd07fb783118016a1657cc2afc4725da
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2023-06-12 04:00:34 +0200

    Add Male random event

--- Src/Person/Male/Male.cpp
+++ Src/Person/Male/Male.cpp
@@ -2,15 +2,17 @@
 
 Male::Male(uint32_t floors_number, uint32_t current_floor)
 {
+    specifier = male;
     set_floor(floors_number, current_floor);
 }
 
-void Male::iteration()
+uint32_t Male::event(uint32_t none)
 {
-    return;
-}
-
-void Male::event()
-{
-    return;
+    if(get_person_random_event_chance())
+    {
+        turn_off_random_event();
+        uint32_t iterations = (rand() % 3) + 1; // <1;3> not <0;2> -> number of iterations lost by male event
+        return iterations;
+    }
+    else return 0; // if not -> return 0 iterations to be made
 }
--- Src/Person/Male/Male.h
+++ Src/Person/Male/Male.h
@@ -2,10 +2,9 @@
 #include "../Person.h"
 
 class Male: public Person {
-    special_event_specifier specifier = male;
 public:
 
     Male(uint32_t, uint32_t);
-    void iteration() override;
-    void event() override;
+    void event() override {};
+    uint32_t event(uint32_t) override;  
 };
